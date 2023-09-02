0248102198bbc3b0946f532f14117f2c05f1f61d
simon place <psi.c0d3@gmail.com>
2023-09-02 01:23:59 +0100

    Update thousands_test.go

--- thousands_test.go
+++ thousands_test.go
@@ -3,8 +3,10 @@ package thousands
 import "fmt"
 
 func ExampleThousands() {
-	fmt.Printf("%1.0[1]v\n%1.1[1]s\n%-1.2[1]s\n%#[1]s\n%#1.1[1]s\n", Int(1000000000))
+	fmt.Printf("%s\n%#[1]s\n%1.0[1]v\n%1.1[1]s\n%-1.2[1]s\n%#[1]s\n%#1.1[1]s\n", Int(1000000000))
 	// Output:
+	// 1,000,000,000
+	// 1,000,000,000
 	// 1 000 000 000
 	// 1,000,000k
 	// 1.000M
@@ -13,11 +15,14 @@ func ExampleThousands() {
 }
 
 func ExampleThousandsNegatives() {
-	fmt.Printf("%1.0v\n%1.1[1]v\n%-1.2[1]s\n%#[1]s\n%#1.1[1]s\n", Int(-1<<30))
+	fmt.Printf("%s\n%#[1]s\n%1.0[1]v\n%1.1[1]v\n%-1.2[1]s\n%#[1]s\n%#1.1[1]s\n", Int(-1<<30))
 	// Output:
+	// -1,073M
+	// -1,024Mi
 	// -1 073 741 824
 	// -1 073 741k
 	// -1.073M
 	// -1,024Mi
 	// -1,048,576ki
 }
+
