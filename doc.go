5054915c49e58b89a076a593579da4a8f0c94867
simon place <psi.c0d3@gmail.com>
2023-09-02 01:26:15 +0100

    Create doc.go

---
+++ doc.go
@@ -0,0 +1,5 @@
+// thousands.Int is an int that implements fmt.Formatter with separated digits.
+// also it can replace a number of 3 digit trailing groups with the appropriate suffix letter.
+// automatically varying the number replaced to retain precision.
+package thousands
+
